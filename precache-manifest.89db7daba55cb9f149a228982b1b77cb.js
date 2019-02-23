8b3a909772298a1fa7896bb35bd4e59ba66bd1c5
Shubham Dhage <shubhamdhage930@gmail.com>
2019-02-23 08:03:37 +0530

    Updates

---
+++ precache-manifest.89db7daba55cb9f149a228982b1b77cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "52ee0804cbb885bb398fb62ee81a70c8",
+    "url": "/myportfolio/static/media/dpp.52ee0804.png"
+  },
+  {
+    "revision": "01a2cde5509f2f4750d6",
+    "url": "/myportfolio/static/js/runtime~main.01a2cde5.js"
+  },
+  {
+    "revision": "4e5c85a7011106875246",
+    "url": "/myportfolio/static/js/main.4e5c85a7.chunk.js"
+  },
+  {
+    "revision": "cfdff3be54c18af692fe",
+    "url": "/myportfolio/static/js/2.cfdff3be.chunk.js"
+  },
+  {
+    "revision": "4e5c85a7011106875246",
+    "url": "/myportfolio/static/css/main.f7fb0f37.chunk.css"
+  },
+  {
+    "revision": "cfdff3be54c18af692fe",
+    "url": "/myportfolio/static/css/2.30131d07.chunk.css"
+  },
+  {
+    "revision": "ff39e2f1cf93713ef2f35137fc043425",
+    "url": "/myportfolio/index.html"
+  }
+];
